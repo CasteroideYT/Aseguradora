545aeb4551c5af233edfc7a70e610f617bf46fc6
Camilo Sebastian Espinoza <camilotvasteroide@gmail.com>
2017-09-22 05:32:34 -0300

    Revert "V- 1.1"
    This reverts commit f0ca31434a13155c682fc88fa6d741228a5c936e.

--- app/src/main/java/cl/espinoza/aseguradora/MostrarDatos.java
+++ app/src/main/java/cl/espinoza/aseguradora/MostrarDatos.java
@@ -1,17 +1,14 @@
 package cl.espinoza.aseguradora;
 
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 
 public class MostrarDatos extends AppCompatActivity {
     TextView    tvDatos;
     TextView    tvValor;
-    ImageView   imResultado;
     String      ppu;
     String      marca;
     String      modelo;
@@ -22,7 +19,7 @@ public class MostrarDatos extends AppCompatActivity {
     int         anioActual;
     int         antiguedad;
     double      valorUF;
-    String      valorSeguro;
+    double      valorSeguro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +27,6 @@ public class MostrarDatos extends AppCompatActivity {
         setContentView(R.layout.activity_mostrar_datos);
         tvDatos         = (TextView) findViewById(R.id.txv_DatosAuto);
         tvValor         = (TextView) findViewById(R.id.txv_ValorSeguro);
-        imResultado     = (ImageView)findViewById(R.id.imv_Estado);
         //Se obtienen los datos capturados en MainActivity
         Bundle bundle   = getIntent().getExtras();
         ppu             = bundle.getString("ppu");
@@ -40,47 +36,38 @@ public class MostrarDatos extends AppCompatActivity {
         anioActual      = bundle.getInt("anioActual");
         antiguedad      = obtnenerAntiguedad(anioActual,anioVehiculo);
         estadoVehiculo  = determinarEstadoVehiculo(antiguedad);
-        valorUF         = 26642.59; //Valor de UF a la fecha 22-09-2017 04:41 AM
+        valorUF         = 26624.85; //Valor de UF a la fecha 12-09-2017 02:45 AM
         valorSeguro     = calcularValorSeguro(antiguedad, valorUF);
         //Creacion del mensaje a mostrar
         datosVehiculo   = String.format(getResources().getString(R.string.mensaje_resultado),marca,modelo,ppu,antiguedad,estadoVehiculo);
         datosSeguro     = String.format(getResources().getString(R.string.valor_seguro),valorSeguro);
         //Se muestra el mensaje con los datos del vehiculo
-        if (estadoVehiculo.equals("ES ASEGURABLE")){
-            imResultado.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.valido));
-        }else{
-            imResultado.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.novalido));
-        }
         tvDatos.setText(datosVehiculo);
         tvValor.setText(datosSeguro);
     }
 
     /* Calcula el valor del seguro, multiplicando 0,1 UF por Años de antiguedad,
     y esto transladandolo a pesos chilenos multiplicando por el valor de la uf */
-    public String calcularValorSeguro (int antiguedad, double uf){
-        String stvalor;
+    public double calcularValorSeguro (int antiguedad, double uf){
         double valor;
-        if(antiguedad <= 10){
+        if(antiguedad < 10){
             valor = (antiguedad * 0.1);
             valor = valor * uf;
         }else {
             valor = 0;
         }
-        stvalor =String.format("%3f",valor);
-        return stvalor;
+
+        return valor;
     }
 
     public int obtnenerAntiguedad(int anioActual, int anioVehiculo){
         int diferencia;
         diferencia = anioActual - anioVehiculo;
-        if (diferencia <= 1){
-            diferencia = 1;
-        }
         return diferencia;
     }
 
     public String determinarEstadoVehiculo(int antiguedad){
-        if (antiguedad <= 10){
+        if (antiguedad < 10){
             return "ES ASEGURABLE";
         }else {
             return "NO ES ASEGURABLE";
